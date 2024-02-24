3fe63660070520fb04e2c19f6209fe340553a5f7
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-02-24 10:20:19 -0300

    aula2.java

--- aula2.java
+++ aula2.java
@@ -2,6 +2,10 @@ package aula2;
 
 public class aula2 { 
 	public static void mais(String[] args) { 
-	//espaço reservado na memoria devidamente indentificado para armazenar valor
+	//espaço reservado na memoria devidamente indentificado para armazenar valor 
+
+	final double VALOR = 3.1415;   
+	System.out.println("O valor de pi é de " + VALOR); 
+
 } 
  }

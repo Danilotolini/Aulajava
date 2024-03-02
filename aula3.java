2b6a1f915e8eef2cafea991ccb34fda6a310c743
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-03-02 09:19:36 -0300

    aula3.java

--- aula3.java
+++ aula3.java
@@ -1,1 +1,16 @@
+package aula3;
+
+import javax.swing.JOptionPane;
+
+public class aula3 {
+	public static void main (String[] args) { 
+		int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número "));   
+		int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite um segundo número "));   
+		if(numero > numero2) {
+		JOptionPane.showMessageDialog(null, "O número " + numero + " é maior que o numero " + numero2); 
+		} else { 
+		JOptionPane.showMessageDialog(null, "O número " + numero2 + " é maior que o numero " + numero);
+		}
+	}
+}
 

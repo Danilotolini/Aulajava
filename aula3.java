05d67acf7c78765efa3c349c9d141272ae19dae6
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-03-02 09:47:47 -0300

    aula3.java

--- aula3.java
+++ aula3.java
@@ -32,3 +32,19 @@ public class pareimpar {
 	}
 } 
 */
+/* 
+package aula3;
+
+public class ifelse2 {
+	public static void main(String[] args) {  
+		int x = 16; 
+		if(x < 15) { 
+			System.out.println("x é menor que 15"); 
+			System.out.println("O valor de x é " + x); 
+		}else { 
+			System.out.println("x é maior ou igual a 15"); 
+			System.out.println("O valor de x é " + x); 
+		}
+	}
+}
+*/

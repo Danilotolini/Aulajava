172af12eea32e5564528975f0c66029712629d0c
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-03-09 10:32:16 -0300

    aula4.java

--- aula4.java
+++ aula4.java
@@ -73,3 +73,22 @@ public class copa {
 	} 
 }
 */
+/* 
+package aula4;
+
+import javax.swing.JOptionPane;
+
+public class anobissexto {
+
+	public static void main(String[] args) { 
+		int ano = Integer.parseInt(JOptionPane.showInputDialog("Digite o ano"));
+		// for(int ano=0; ano==2024; ano++) { 
+			if((ano%4==0 && (ano%100)!=0) || (ano%400==0))
+		JOptionPane.showMessageDialog(null, "Este ano é um ano bissexto");		  
+			else 
+		JOptionPane.showMessageDialog(null, "Este ano não é um ano bissexto");		
+		}
+
+	}  
+ */ 
+/*

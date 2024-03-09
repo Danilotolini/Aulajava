2350053f52c04344ccb317e878ef91462dbd1d02
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-03-09 09:17:07 -0300

    aula4.java

--- aula4.java
+++ aula4.java
@@ -1,1 +1,25 @@
+/*
+package aula4;
 
+import javax.swing.JOptionPane;
+
+public class aula4 {
+	public static void main(String[] args) { 
+		int mes = Integer.parseInt(JOptionPane.showInputDialog("Digite um mês em numerico")); 
+		switch(mes) { 
+		case 1: JOptionPane.showMessageDialog(null, "O mês de janeiro tem 31 dias"); break;
+		case 2: JOptionPane.showMessageDialog(null, "O mês de fevereiro tem 29 dias"); break; 
+		case 3: JOptionPane.showMessageDialog(null, "O mês de março tem 31 dias"); break;
+		case 4: JOptionPane.showMessageDialog(null, "O mês de abril tem 30 dias"); break;
+		case 5: JOptionPane.showMessageDialog(null, "O mês de maio tem 31 dias"); break;
+		case 6: JOptionPane.showMessageDialog(null, "O mês de junho tem 30 dias"); break; 
+		case 7: JOptionPane.showMessageDialog(null, "O mês de julho tem 31 dias"); break;  
+		case 8: JOptionPane.showMessageDialog(null, "O mês de agosto tem 31 dias"); break;  
+		case 9: JOptionPane.showMessageDialog(null, "O mês de setembro tem 30 dias"); break; 
+		case 10: JOptionPane.showMessageDialog(null, "O mês de outubro tem 31 dias"); break;
+		case 11: JOptionPane.showMessageDialog(null, "O mês de novembro tem 30 dias"); break; 
+		case 12: JOptionPane.showMessageDialog(null, "O mês de dezembro tem 31 dias"); break;
+		}
+	}
+}
+*/

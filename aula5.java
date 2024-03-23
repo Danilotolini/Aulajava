8fcc8f00adea567d73185b673d752c78bd42e789
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-03-23 10:44:29 -0300

    aula5.java

---
+++ aula5.java
@@ -0,0 +1,26 @@
+/*
+package aula5;
+
+public class Carro {
+	String cor; 
+	int anoFabricacao; 
+	String modelo; 
+	String marca;
+}
+*/ 
+/* 
+package aula5;
+
+public class Carroapp {
+	public static void main(String[] args) {
+		Carro amg = new Carro(); 
+		amg.marca = "Mercedes"; 
+		amg.modelo = "CLA35"; 
+		amg.anoFabricacao = 2021; 
+		amg.cor = "verde";  
+		
+System.out.println( "Marca: " + amg.marca  + "Modelo: " 
++ amg.modelo + "Ano de Fabricação: " +  amg.anoFabricacao + "cor: " + amg.cor);
+	}
+}
+*/

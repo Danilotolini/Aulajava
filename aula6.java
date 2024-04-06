96208d30711106f573239e6a7c2f2eabd5b7322b
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-04-06 10:05:17 -0300

    aula6.java

---
+++ aula6.java
@@ -0,0 +1,39 @@
+/*
+package aula6;
+
+import javax.swing.JOptionPane;
+
+public class Funcionario {
+	private String nome, sobrenome, cargo; 
+	private	float salario;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public float getSalario() {
+		return salario;
+	}
+	public void setSalario(float salario) { 
+		if(salario>1500)
+		this.salario = salario; 
+		else 
+			JOptionPane.showMessageDialog(null, "O salário deve ser maior que o valor do salario minimo");
+	}  
+	
+	
+}
+*/

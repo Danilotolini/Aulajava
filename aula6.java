7c18ea4f9e7e1615d5bfe3e979518dd0673d1a16
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-04-06 11:30:03 -0300

    aula6.java

--- aula6.java
+++ aula6.java
@@ -92,3 +92,64 @@ public class modeloscarros{
 	}
 }
 */
+/* 
+package aula05.lab.cap5Lab2.carro;
+
+public class Garagem {
+	private Carro carroPasseio = new Carro();
+	private Carro carroUtilitario = new Carro();
+	public Carro getCarroPasseio() {
+		return carroPasseio;
+	}
+	public void setCarroPasseio(Carro carroPasseio) {
+		this.carroPasseio = carroPasseio;
+	}
+	public Carro getCarroUtilitario() {
+		return carroUtilitario;
+	}
+	public void setCarroUtilitario(Carro carroUtilitario) {
+		this.carroUtilitario = carroUtilitario;
+	}
+	
+	
+}
+
+
+====================================================================
+package aula05.lab.cap5Lab2.carro;
+public class Cap5_Lab2 {
+	public static void main(String[] args) {		
+		
+		Carro c = new Carro();
+		
+		c.setCor("vermelho");
+		c.setModelo("VW Jetta");
+		c.setPotencia(2.3);
+		
+		Garagem g = new Garagem();		
+		g.setCarroPasseio(c);
+		
+		Carro c1 = new Carro();
+		
+		c1.setCor("branco");
+		c1.setModelo("Renault Boxer");
+		c1.setPotencia(3.8);
+		
+		g.setCarroUtilitario(c1);
+		
+		System.out.println("Carro de Passeio: ");
+		System.out.println("Cor: " + g.getCarroPasseio().getCor());
+		System.out.println("Modelo: " + g.getCarroPasseio().getModelo());
+		System.out.println("Potência: " + g.getCarroPasseio().getPotencia());
+		
+		System.out.println("---------------------------------------");
+		
+		System.out.println("Carro Utilitário: ");
+		System.out.println("Cor: " + g.getCarroUtilitario().getCor());
+		System.out.println("Modelo: " + g.getCarroUtilitario().getModelo());
+		System.out.println("Potência: " + g.getCarroUtilitario().getPotencia());
+		
+	}
+
+} 
+*/

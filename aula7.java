646aec97c3c903c1db6c93d1db084aa9209e66d5
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-04-13 11:26:50 -0300

    aula7.java

--- aula7.java
+++ aula7.java
@@ -75,4 +75,60 @@ public class Contatoslista {
 				contatos[i].listarContato(contatos[i]);
 			}
 		}
-	} 
+	}  
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+public class Calculadora {
+	// M�todos criados durante o Cap 07 e Lab 1:
+	public int somar(int valor1, int valor2) {
+		int resultado;
+		resultado = valor1 + valor2;
+		return resultado;
+	}
+	
+	public int subtrair (int valor1, int valor2) {
+		int resultado = valor1 - valor2;
+		return resultado;
+	}
+	
+	public int multiplicar (int valor1, int valor2) {
+		int resultado = valor1 * valor2;
+		return resultado;
+	}
+	
+	public int dividir (int valor1, int valor2) {
+		int resultado = valor1 / valor2;
+		return resultado;
+	}
+	
+	// M�todos sobrecarregados criados no Lab 2
+	public double subtrair(double valor1, double valor2) {
+		double resultado;
+		resultado = valor1 - valor2;
+		return resultado;
+	}
+	
+	public double subtrair(int valor1, double valor2) {
+		double resultado;
+		resultado = valor1 - valor2;
+		return resultado;
+	}
+	
+	public double subtrair(double valor1, int valor2) {
+		double resultado;
+		resultado = valor1 - valor2;
+		return resultado;
+	}
+} 
+==================================================================================================================================================
+public class Cap6_Lab2 {
+	public static void main(String[] args) {		
+		Calculadora calc = new Calculadora();
+		System.out.println("Métodos subtrair sobrecarregados:");
+		System.out.println("Chamando a versão de subtrair com "
+				+ "2 double: " + calc.subtrair(6.2, 3.1));
+		System.out.println("Chamando a versão de subtrair com "
+				+ "1 double e um int: " + calc.subtrair(6.2, 2));
+		System.out.println("Chamando a versão de subtrair com "
+				+ "1 int e um double: " + calc.subtrair(6, 3.2));		
+	}
+}

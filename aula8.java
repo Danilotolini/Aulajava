228dd4ce916f1406efea6b3844bffec69fc15555
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-04-20 09:48:27 -0300

    aula8.java

---
+++ aula8.java
@@ -0,0 +1,36 @@
+package aula8;
+
+public class Pessoa {
+String nome;  
+int peso;   
+double altura;  
+
+
+
+} 
+=========================================================================================================================================
+package aula8;
+
+public class aula8 {
+	public static void main(String[] args) { 
+		Pessoa pessoas[] = new Pessoa[2]; // declara que pessoas vai armazenar 2 elementos do tipo Pessoa
+		pessoas[0] = new Pessoa(); // cria inst�ncia para ocorr�ncia 0
+		pessoas[0].altura = 1.68;
+		pessoas[0].peso = 55;
+		pessoas[0].nome = "Juca";
+		
+		System.out.println("Nome: " + pessoas[0].nome 
+				+ "Altura: " + pessoas[0].altura
+				+ "Peso: " + + pessoas[0].peso);
+		
+		pessoas[1] = new Pessoa(); // cria inst�ncia para ocorr�ncia 1
+		pessoas[1].altura = 1.77;
+		pessoas[1].peso = 60;
+		pessoas[0].nome = "Maria";
+		
+		System.out.println("Nome: " + pessoas[1].nome 
+				+ "Altura: " + pessoas[1].altura
+				+ "Peso: " + + pessoas[1].peso);
+	} 
+} 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
